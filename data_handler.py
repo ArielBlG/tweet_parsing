8844bcb73c843143d094e4f1748dc5629d9bc19b
ArielBlG <62558598+ArielBlG@users.noreply.github.com>
2024-12-04 14:45:33 +0200

    Add files via upload

--- data_handler.py
+++ data_handler.py
@@ -1,8 +1,10 @@
 import json
 import pandas as pd
+import numpy as np
 from typing import List, Dict, Union
 import ast
 from json import JSONDecodeError
+from pyspark.sql.types import StructType, StructField
 
 
 def read_ndjson(path: str, return_as_df=False) -> Union[List[Dict], pd.DataFrame]:
@@ -16,6 +18,33 @@ def read_ndjson(path: str, return_as_df=False) -> Union[List[Dict], pd.DataFrame
     if return_as_df:
          return pd.DataFrame(list_to_return)
     return list_to_return
+
+
+def fillna_method(col):
+    """
+    Custom method to fill NaN values based on column data type.
+    - Returns '' for strings
+    - Returns 0 for numbers
+    - Returns [] for lists
+    """
+    if col.dtype == np.object:  # For object columns
+        # Check if the column contains lists
+        if col.dropna().apply(lambda x: isinstance(x, list)).any():
+            return col.apply(lambda x: [] if pd.isna(x) else x)
+        else:
+            return col.fillna('')
+    elif np.issubdtype(col.dtype, np.number):  # For numeric columns
+        return col.fillna(0)
+    else:
+        return col.fillna(None) 
+
+def read_ndjson_spark(path: str, schema_path: str, spark):
+     with open(schema_path) as f:
+          d = json.load(f)
+     schemaNew = StructType.fromJson(d)
+     df = spark.read.schema(schemaNew).json(path)
+     # df = spark.read.json(path)
+     return df
 
 
 def preprocess_data(data: pd.DataFrame) -> pd.DataFrame:
